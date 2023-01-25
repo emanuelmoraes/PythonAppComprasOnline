34825f01b8c9313bf8ed2431626a5cf215b190aa
EmanuelMoraes <emanueljsmoraes@gmail.com>
2023-01-24 21:27:25 -0300

    Modificações para utilizar a biblioteca Material do kivy.

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
 import kivy
 kivy.require("2.0.0")
-from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
 
-class WindowManager(ScreenManager):
-    pass
-
-class TelaInicial(Screen):
-    pass
-
-class TelaInicial2(Screen):
-    pass
-
-class TelaUsuario(Screen):
-    pass
-
-class TelaLogin(Screen):
+class TelaLogin(MDScreen):
     def login_press(self):
         user = self.ids.lbLogin.text
         senha = self.ids.lbSenha.text
         if (user == 'eu' and senha == '123'):
-            self.manager.current = 'telainicial2'
+            self.manager.current = 'telaproduto'
         else:
             self.ids.lbResposta.text = 'Login ou senha inválido'
 
-class MainApp(App):
-    pass
-
-from kivy.core.window import Window
-Window.size = (500, 600)
+class MainApp(MDApp):
+    def callback(self, tela):
+        self.root.current = str(tela)
 
 janela = MainApp()
+janela.title = 'Carrinho compras online'
 janela.run()
